9720d5007d31582a8f3d60764358ac71db7ce4f7
neWKing1 <duonggiaothien@gmail.com>
2024-06-24 10:52:14 +0700

    connect serivce and setup route with api gate way

--- task-service/src/main/java/com/duongdh/taskservice/TaskServiceApplication.java
+++ task-service/src/main/java/com/duongdh/taskservice/TaskServiceApplication.java
@@ -3,12 +3,13 @@ package com.duongdh.taskservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class TaskServiceApplication {
 
 	@Bean
--- task-service/src/main/java/com/duongdh/taskservice/service/ApiClient.java
+++ task-service/src/main/java/com/duongdh/taskservice/service/ApiClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "OWNER-SERVICE")
+@FeignClient(name = "OWNER-SERVICE")
 public interface ApiClient {
     // Build get Owner REST API
     @GetMapping("api/owners/{owner-code}")
